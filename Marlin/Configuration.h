c5bd93fed9617d597607ee7c551d30797055f12a
Miroslav Sustek <sustmidown@centrum.cz>
2019-10-29 21:36:08 +0100

    Calibrate PID with titanium (TC4) all metal heat break

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -473,9 +473,9 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-3
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  #define DEFAULT_Kp 18.04
+  #define DEFAULT_Ki 1.29
+  #define DEFAULT_Kd 63.26
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
